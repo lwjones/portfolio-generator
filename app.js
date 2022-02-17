dbfc83d8d2f147555c03cfaf348bfa402da17f56
Lawrence Jones <git@lwjon.es>
2022-02-16 22:10:29 -0800

    Generate site pages with promises

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site')
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
@@ -131,14 +131,22 @@ Add a new Project
       }
     });
 }
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log("Portfolio complete! Check out index.html to see the output");
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
